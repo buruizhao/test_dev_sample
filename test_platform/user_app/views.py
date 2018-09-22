21112cfc76e76ebb1e568b55846fbb8c53e2d32a
buruizhao <buruizhao@163.com>
2018-09-22 18:22:09 +0800

    添加登陆页面及简单处理逻辑

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+#登陆首页
+# def index(request):
+#     return HttpResponse("Hello world！")
+
+def index(request):
+    return render(request,"index.html")
+
+#登陆逻辑处理
+def login_action(request):
+    if request.method == 'GET':
+        username = request.GET.get('username','')
+        password = request.GET.get('password','')
+
+        if username == '' and password == '':
+            return render(request, 'index.html', {'error':'用户名或密码不能为空，请重新登录！'})
+
+        if username == 'admin' and password == 'admin':
+            return HttpResponse('登陆成功')
+        else:
+            return render(request, 'index.html', {'error':'用户名或密码错误，请重新登录！'})
