089b30646effc51a04924ea152e697762581ae8a
buruizhao <buruizhao@163.com>
2018-09-26 23:53:41 +0800

    实现用户登录功能

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import auth
 
 # Create your views here.
 
@@ -12,14 +13,23 @@ def index(request):
 
 #登陆逻辑处理
 def login_action(request):
-    if request.method == 'GET':
-        username = request.GET.get('username','')
-        password = request.GET.get('password','')
+    # if request.method == 'GET':
+    #     username = request.GET.get('username','')
+    #     password = request.GET.get('password','')
 
-        if username == '' and password == '':
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+
+        if username == '' or password == '':
             return render(request, 'index.html', {'error':'用户名或密码不能为空，请重新登录！'})
-
-        if username == 'admin' and password == 'admin':
-            return HttpResponse('登陆成功')
         else:
-            return render(request, 'index.html', {'error':'用户名或密码错误，请重新登录！'})
+            user = auth.authenticate(username = username,password = password)
+
+            # if username == 'admin' and password == '123456':
+
+            if user is not None:
+                auth.login(request,user)
+                return render(request, "project_manage.html")
+            else:
+                return render(request, 'index.html', {'error': '用户名或密码错误，请重新登录！'})
